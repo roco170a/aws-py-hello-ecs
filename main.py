70543e53bf8d07f82a0fea25f5aecf443d42c2df
Roberto Corona <roco@gmail.com>
2025-08-20 12:51:30 -0600

    comentario 1

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 async def read_root(): 
     return {"message": "Hello, World!"}
 
+#Set up the server to run on port 8000
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
