0ee1171e455b60ebb53b880c8ff132f9a87cc34d
Roberto Corona <roco@gmail.com>
2025-08-20 21:24:15 -0600

    script 7

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# Asegúrate de que el directorio tenga los permisos correctos
+sudo chown -R ubuntu:ubuntu /home/ubuntu/mi-fastapi-app
+
 cd /home/ubuntu/mi-fastapi-app
 # Crea un entorno virtual con Python 3.12
 python3.12 -m venv venv
 # Activa el entorno virtual
 source venv/bin/activate
-# Instala las dependencias usando el pip del entorno virtual
-pip install -r requirements.txt
+
+# Instala las dependencias dentro del entorno virtual
+pip install --break-system-packages -r requirements.txt
