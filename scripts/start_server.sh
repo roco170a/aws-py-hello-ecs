4999264f849adaebfce48a2a1d083a6e1c462b58
Roberto Corona <roco@gmail.com>
2025-08-20 21:41:37 -0600

    script 8

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,5 @@ cd /home/ubuntu/mi-fastapi-app
 # Activa el entorno virtual
 source venv/bin/activate
 # Ejecuta Uvicorn usando el Python del entorno virtual
-python -m uvicorn main:app --host 0.0.0.0 --port 8000 --reload &
+python -m uvicorn main:app --host 0.0.0.0 --port 8000 
 
